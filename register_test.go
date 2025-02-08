1d792a9199d58c6a0f74de3fd2066ed32d49cb2a
Dmitrii D. <ddemin@users.noreply.github.com>
2025-02-08 19:32:34 +0400

    Update register_test.go

--- register_test.go
+++ register_test.go
@@ -1,4 +1,4 @@
-package sqlite3
+package sqlite
 
 import (
 	_ "embed"
@@ -11,5 +11,5 @@ import (
 var script string
 
 func TestIntegration(t *testing.T) { //nolint:paralleltest
-	sqltest.RunScript(t, "sqlite3", "integration-test.db", script)
+	sqltest.RunScript(t, "sqlite", "integration-test.db", script)
 }
